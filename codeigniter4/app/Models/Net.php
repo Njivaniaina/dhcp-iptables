5f91fee3f9f3de519bf513e5ce1a91f5f9722d0b
Njivaniaina <fenosoanjivaniaina@gmail.com>
2024-05-03 11:30:20 +0300

    Debug dhcp front

--- codeigniter4/app/Models/Net.php
+++ codeigniter4/app/Models/Net.php
@@ -73,13 +73,46 @@ class Net
 
     }
 
+    public function verification_fichier($nom_fichier)
+    {
+        if(is_file($nom_fichier))
+        {
+            echo "le fichier existe\n";
+            return true;
+        }
+        else{
+            echo "fichier introuvable\n";
+            return false;
+        }
+    }
+
     public function getSubnet()
     {
         //retourne un tableau concernant tout les subnet trouve dans le fichier
         $dhcp = fopen("/etc/dhcp/dhcpd.conf","r");
         while($row = fgets($dhcp))
         {
-            if(strstr($row,"subnet") && !strstr($row[0],"#"))
+            if(strstr($row,"subnet") && $row[0]!="#")
+            {
+                $p1 = $row;
+                $p2 = fgets($dhcp);
+                $tableau = [
+                    "subnet" => explode(" ",$p1)[1],
+                    "netmask" => explode(" ",$p1)[3],
+                    "range" => [
+                                    explode(" ",$p2)[3],
+                                    str_replace(";","",explode(" ",$p2)[4])
+                                ],
+                ];
+                fclose($dhcp);
+                return $tableau;
+            }
+        }
+        $this->addSubnet("10.254.239.0","255.255.255.224","10.254.239.10","10.254.239.20");
+        $dhcp = fopen("/etc/dhcp/dhcpd.conf","r");
+        while($row = fgets($dhcp))
+        {
+            if(strstr($row,"subnet") && $row[0]!="#")
             {
                 $p1 = $row;
                 $p2 = fgets($dhcp);
@@ -130,9 +163,9 @@ class Net
             {
                 $nom_hote = explode(" ",$row)[1];
                 $row = fgets($fichier);
-                $mac = str_replace(";","",explode(" ethernet ",$row)[1]);
+                $mac = str_replace("\n","",str_replace(";","",explode(" ethernet ",$row)[1]));
                 $row = fgets($fichier);
-                $ip = explode(" ",$row);
+                $ip = explode(";",explode(" ",$row)[3])[0];
                 $tableau = [
                     "nom_hosts" => $nom_hote,
                     "mac" => $mac,
@@ -175,7 +208,8 @@ class Net
         // $this->supprimerSubnet();
         // if($error->test_subnet($subnet,$netmask,$min,$max))
         // {
-        $this->addSubnet($subnet,$netmask,$min,$max);
+        if(strlen($subnet) && strlen($netmask) && strlen($min) && strlen($max))
+            $this->addSubnet($subnet,$netmask,$min,$max);
         // }$this->removeSubnetWithLines();
         // $this->supprimerSubnet();
     }
@@ -293,9 +327,11 @@ class Net
     public function addHost($nom,$mac,$ip)
     {
         $fichier = fopen("/etc/dhcp/dhcpd.conf","a");
-
-            $host = "host $nom {\n  hardware ethernet $mac;\n  fixed-address $ip;\n}\n";
-            fputs($fichier,$host);
+            if($nom!="" && $mac!="" && $ip!="")
+            {
+                $host = "host $nom {\n  hardware ethernet $mac;\n  fixed-address $ip;\n}\n";
+                fputs($fichier,$host);
+            }
 
         fclose($fichier);
     }
@@ -317,20 +353,6 @@ class Net
     public function modifHost($nom,$mac,$ip)
     {
         $this->supprimerHost($nom);
-        // $num_host = $this->getLigneHost($nom);
-        // $fichier = fopen("/etc/dhcp/dhcpd.conf","r+");
-        // $num_ligne = 1;
-        // while($row = fgets($fichier))
-        // {
-        //     if($num_ligne == $num_host)
-        //     {
-        //         break;
-        //     }
-        //     $num_ligne++;
-        // }
-        // $host = "host $nom {\n  hardware ethernet $mac;\n  fixed-address $ip;\n}\n";
-        // fputs($fichier,$host);
-        // fclose($fichier);
         $this->addHost($nom,$mac,$ip);
         $this->range();
     }
@@ -351,10 +373,51 @@ class Net
         {
             $nom = str_replace("\n","",$h['nom_hosts']);
             $mac = str_replace("\n","",$h['mac']);
-            $ip = explode(";",$h['ip'][3])[0];
+            // $ip = explode(";",$h['ip'][3])[0];
+            $ip = $h["ip"];
             $this->addHost($nom,$mac,$ip);
         }
         fclose($fichier);
     }
 
+    public function delHostWithLines($num_ligne)
+    {
+        $nom_fichier = "/etc/dhcp/dhcpd.conf";
+        for($i = 0;$i < 4;$i++)
+        {
+            $commande = "sudo sed -i '".$num_ligne."d' $nom_fichier";
+            shell_exec($commande);
+        }
+    }
+
+    public function modif_host_lines($l,$nom,$mac,$ip)//pour modifier une personne a une ligne specifie
+    {//il restera sur le meme ligne si tout les host sont alignes !
+        $l--;
+        $num_ligne = $this->getLigneHost($nom);
+        $tab = $this->getHost();
+        $ligne = $this->getLinesHost();
+        $tab[$l]['nom_hosts'] = $nom;
+        $tab[$l]['mac'] = $mac;
+        $tab[$l]['ip'] = $ip;
+        $this->removeHostWithLines();
+        foreach($tab as $h)
+        {
+            $this->addHost($h['nom_hosts'],$h['mac'],$h['ip']);
+        }
+        $this->range();
+    }
+
+    public function getHostWithLines($num_ligne)
+    {
+        $tab = $this->getHost();
+        return $tab[$num_ligne-1];
+    }
 }
+// $n = new Net();
+// $n->verification_fichier("/etc/dhcp/dhcpd.conf");
+// $n->modif_host_lines(1,"junior","467543","123");
+// $n->removeHostWithLines();
+// $n->addSubnet("mille","467543","123","hello");
+// $n->removeSubnetWithLines();
+// $n->range();
+?>
