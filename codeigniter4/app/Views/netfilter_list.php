38aaf631b85df341a20dce5f657bfbd0a7f63479
Hajatiana61 <randrianantenainahajatiana61@gmail.com>
2024-05-08 15:57:13 +0300

    upgrade all

--- codeigniter4/app/Views/netfilter_list.php
+++ codeigniter4/app/Views/netfilter_list.php
@@ -20,7 +20,7 @@
   <body>
     <header>
         <div class="logo" id="btn-menu">
-        <i class="bx menu-icon"><img src="/assets/menu.svg" alt="menu"></i>
+          <i class="bx menu-icon"><img src="/assets/menu.svg" alt="menu"></i>
         </div>
         <div class="title">
             <h1>NETFILTER</h1>
--- codeigniter4/app/Views/netfilter_police.php
+++ codeigniter4/app/Views/netfilter_police.php
@@ -26,83 +26,96 @@
     </header>
     
     <nav>
-        <div class="sidebar">
-            <div></div>
-            <div class="sidebar-content">
-              <ul class="lists">
-                <li class="list">
-                  <a href="/netfilter" class="nav-link">
-                    <span class="link">Home</span>
-                  </a>
-                </li>
-                <li class="list">
-                  <a href="/netfilter/list" class="nav-link">
-                    <span class="link">List</span>
-                  </a>
-                </li>
-                <li class="list">
-                  <a href="/netfilter/add" class="nav-link">
-                    <span class="link">Add</span>
-                  </a>
-                </li>
-                <li class="list">
-                  <a href="/netfilter/police" class="nav-link">
-                    <span class="link">Policy</span>
-                  </a>
-                </li>            
-              </ul>
-            </div>
+      <div class="sidebar">
+        <div></div>
+        <div class="sidebar-content">
+          <ul class="lists">
+            <li class="list">
+              <a href="/netfilter" class="nav-link">
+                <span class="link">Home</span>
+              </a>
+            </li>
+            <li class="list">
+              <a href="/netfilter/list" class="nav-link">
+                <span class="link">List</span>
+              </a>
+            </li>
+            <li class="list">
+              <a href="/netfilter/add" class="nav-link">
+                <span class="link">Add</span>
+              </a>
+            </li>
+            <li class="list">
+              <a href="/netfilter/police" class="nav-link">
+                <span class="link">Policy</span>
+              </a>
+            </li>            
+          </ul>
         </div>
+      </div>
     </nav>
     <section class="overlay"></section>
     
     <main>
-        <div class="content">
-            <div class="content-item">
-                <div class="policy">
-                  <div class="row">
-                    <div class="col-md-8 offset-md-2 card">
-                      <div class="card-header">
-                        <h2>The Policy</h2>
-                      </div>
-                      <div class="card-body">
-                        <form action="./police" method="post">
-                          <div class="row align-self-end p-2 bd-highlight">
-                            <label for="input">INPUT </label>
-                            <select stylename="input" id="input">
-                                <option value="ACCEPT" <?php if($chain[0][1]==="ACCEPT") echo "selected";?>>ACCEPT</option>
-                                <option value="DROP" <?php if($chain[0][1]==="DROP") echo "selected";?>>DROP</option>
-                                <option value="REJECT" <?php if($chain[0][1]==="REJECT") echo "selected";?>>REJECT</option>
-                            </select><br><br>
-                          </div>
-                          <div class="row align-self-end p-2 bd-highlight">
-                            <label for="forward">FORWARD </label>
-                            <select name="forward" id="forward">
-                                <option value="ACCEPT" <?php if($chain[1][1]==="ACCEPT") echo "selected";?>>ACCEPT</option>
-                                <option value="DROP" <?php if($chain[1][1]==="DROP") echo "selected";?>>DROP</option>
-                                <option value="REJECT" <?php if($chain[1][1]==="REJECT") echo "selected";?>>REJECT</option>
-                            </select><br><br>
-                          </div>
-                          <div class="row align-self-end p-2 bd-highlight">
-                            <label for="output">OUTPUT </label>
-                            <select name="output" id="output" value=<?php echo trim($chain[2][1]);?>>
-                                <option value="ACCEPT" <?php if($chain[2][1]==="ACCEPT") echo "selected";?>>ACCEPT</option>
-                                <option value="DROP" <?php if($chain[2][1]==="DROP") echo "selected";?>>DROP</option>
-                                <option value="REJECT" <?php if($chain[2][1]==="REJECT") echo "selected";?>>REJECT</option>
-                            </select><br><br>
-                          </div>
-                          <div class="row align-self-end p-5 bd-highlight">
-                            <input class="float-end btn btn-success" type="submit" value="Modifier"/>
-                          </div>
-                        </form>
-                    </div>
-                  </div>
-                </div>
+      <div class="content">
+        <div class="content-item">
+          <div class="policy">
+            <div class="row">
+              <div class="policy-col">
+                <button class="list-nav-item page-all page-actif">ALL</button>
+                <button class="list-nav-item page-l1">L1</button>
+                <button class="list-nav-item page-l2">L2</button>
+                <button class="list-nav-item page-l3">L3</button>
+                <button class="list-nav-item page-m1">M1</button>
+                <button class="list-nav-item page-m2">M2</button>
               </div>
             </div>
+           
+            <div class="row">
+              <div class="col-md-8 offset-md-2 card">
+                <div class="card-header">
+                  <h2 class="policy-title">The Policy All</h2>
+                </div>
+                <div class="card-body">
+                  <form action="./police" method="post">
+                    <input type="hidden" name="level" id="level" value="all"/>
+                    <div class="row align-self-end p-2 bd-highlight">
+                      <label for="input">INPUT </label>
+                      <select class="form-select form-select-lg" stylename="input" id="input">
+                          <option value="ACCEPT" <?php if($chain[0][1]==="ACCEPT") echo "selected";?>>ACCEPT</option>
+                          <option value="DROP" <?php if($chain[0][1]==="DROP") echo "selected";?>>DROP</option>
+                          <option value="REJECT" <?php if($chain[0][1]==="REJECT") echo "selected";?>>REJECT</option>
+                      </select><br><br>
+                    </div>
+                    <div class="row align-self-end p-2 bd-highlight">
+                      <label for="forward">FORWARD </label>
+                      <select class="form-select form-select-lg" name="forward" id="forward">
+                          <option value="ACCEPT" <?php if($chain[1][1]==="ACCEPT") echo "selected";?>>ACCEPT</option>
+                          <option value="DROP" <?php if($chain[1][1]==="DROP") echo "selected";?>>DROP</option>
+                          <option value="REJECT" <?php if($chain[1][1]==="REJECT") echo "selected";?>>REJECT</option>
+                      </select><br><br>
+                    </div>
+                    <div class="row align-self-end p-2 bd-highlight">
+                      <label for="output">OUTPUT </label>
+                      <select class="form-select form-select-lg" name="output" id="output" value=<?php echo trim($chain[2][1]);?>>
+                          <option value="ACCEPT" <?php if($chain[2][1]==="ACCEPT") echo "selected";?>>ACCEPT</option>
+                          <option value="DROP" <?php if($chain[2][1]==="DROP") echo "selected";?>>DROP</option>
+                          <option value="REJECT" <?php if($chain[2][1]==="REJECT") echo "selected";?>>REJECT</option>
+                      </select><br><br>
+                    </div>
+                    <div class="row align-self-end p-5 bd-highlight">
+                      <input class="float-end btn btn-success" type="submit" value="Modifier"/>
+                    </div>
+                  </form>
+              </div>
+            </div>
+            
+          </div>
         </div>
+      </div>
     </main>
 
     <script src="../js/script.js"></script>
+    <script src="../js/netfilter.js"></script>
   </body>
 </html>
