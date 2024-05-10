dd3f438fe069990a9fb2f70022c2a7f41bf2489d
tsiory28 <141818450+tsiory28@users.noreply.github.com>
2024-05-10 16:36:51 +0300

    Update netfilter_add.php

--- codeigniter4/app/Views/netfilter_add.php
+++ codeigniter4/app/Views/netfilter_add.php
@@ -13,7 +13,7 @@
       href="https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="<?= base_url('bootstrap/css/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/netfilter_add.css" />
   </head>
   <body>
